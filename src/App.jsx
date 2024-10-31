16a82e225c41690465db47a4c664248decc72bd8
Nasim Azizi <nasim.azizi2012@gmail.com>
2024-10-31 09:55:22 +0430

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,8 @@ const App = () => {
       <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
         <img src="logo.png" alt="" style={{ height:'200px' }} />
       <h1 className='title' style={{ marginTop: '-38px' }}>Welcome To Azizi Soft</h1>
-        <p>Our website is launching soon...</p>
       <div className="box">
-        <h2>Software Solutions Company</h2>
+        <h2>Our website is launching soon...</h2>
       </div>
     </div>
   )
